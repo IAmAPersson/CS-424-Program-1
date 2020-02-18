c5c9c062e5ceb77b19a976859916ec7673932c49
IAmAPersson <iamaperson620@gmail.com>
2020-02-18 01:29:35 -0600

    file partitioning

---
+++ src/types.go
@@ -0,0 +1,31 @@
+/*
+	Phil Lane
+	02/17/2020
+	CS-424-02
+	Mary Allen
+
+	Programming Assignment 1 (Golang)
+*/
+
+package main
+
+type BatterInfo struct {
+	firstName string
+	lastName string
+	plateAppearances uint64
+	atBats uint64
+	singles uint64
+	doubles uint64
+	triples uint64
+	homeRuns uint64
+	walks uint64
+	hitByPitch uint64
+}
+
+type CalculatedBatterInfo struct {
+	firstName string
+	lastName string
+	average float64
+	slugging float64
+	onBase float64
+}
