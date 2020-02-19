e21474b65c0da3add0fcd81adacae7b9166ad3cb
IAmAPersson <iamaperson620@gmail.com>
2020-02-18 18:13:42 -0600

    commenting

--- src/program1.go
+++ src/program1.go
@@ -9,19 +9,26 @@
 
 package main
 
+//Function: main, entrypoint
 func main() {
+	//Get the path of the file to read from
 	path := GetPath()
 
+	//Read all the data from the file
 	data, err := ReadInFile(path)
 	
+	//If there has been an error in reading from the file, exit program
 	if err != nil {
 		return
 	}
 	
+	//Parse file and sort info
 	batters, badlines := ParseInfo(data)
 	batters = PlayerSort(batters)
 	
+	//Calculate data about batters
 	calcData := Calculate(batters)
 	
+	//Output calculated data to the screen
 	FormatData(calcData, badlines)
 }
